5b54486b471d58e161bbae3e9ab8015b93bda60e
Isma Iswandy <seiryu.2i@gmail.com>
2016-10-28 13:20:08 +0700

    turn off ft5x06 debug

--- include/linux/input/ft5x06_ts.h
+++ include/linux/input/ft5x06_ts.h
@@ -299,7 +299,7 @@ struct ft5x06_ts_data {
 
 #define WT_ADD_CTP_INFO   1
 
-#define CTP_DEBUG_ON 1
+#define CTP_DEBUG_ON 0
 #define CTP_DEBUG_FUNC_ON 0
 #define CTP_INFO(fmt, arg...)		   printk("FT5X06-TP-TAG INFO:"fmt"\n", ##arg)
 
